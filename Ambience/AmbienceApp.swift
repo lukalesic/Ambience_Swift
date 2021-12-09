2b9f2b9d588688686a547fba2d18c7c1c1427417
lukalesic <lukalesic20@gmail.com>
2021-12-09 22:04:26 +0100

    Initial Commit

---
+++ Ambience/AmbienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbienceApp.swift
+//  Ambience
+//
+//  Created by Luka Lešić on 09.12.2021..
+//
+
+import SwiftUI
+
+@main
+struct AmbienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
